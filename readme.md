53e86effc4bc649ee153ed310bc43defbf577ea9
aguiarsilva <aguiarsilva@gmail.com>
2021-04-27 10:54:53 -0700

    inicial commit for Redux Project

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Introduction to Redux Project
+
+## Introduction
+This project was created during a Redux course on the Fullstack program from Codecademy. It contains the basics of redux use and reducer function in redux.
+
+## Technology
+- JS
+- Redux
+
+## Status
+The project is completed.
---
+++ reduxRoad.js
@@ -0,0 +1,82 @@
+const initialWagonState = {
+    supplies: 100,
+    distance: 0,
+    days: 0,
+    cash: 200
+}
+
+const roadReducer = (state = initialWagonState, action) => {
+    switch (action.type) {
+        case 'gather': {
+            return {
+                ...state,
+                supplies: state.supplies + 15,
+                distance: state.distance,
+                days: state.days + 1
+            }
+        };
+        case 'travel': {
+            if (state.supplies - (20 * action.payload) < 0) {
+                return state;
+            } else {
+                return {
+                    ...state,
+                    supplies: state.supplies - (20 * action.payload),
+                    distance: state.distance + (10 * action.payload),
+                    days: state.days + action.payload
+                };
+            }
+        }
+        case 'tippedWagon': {
+            return {
+                ...state,
+                supplies: state.supplies - 30,
+                distance: state.distance,
+                days: state.days + 1
+            };
+        }
+        case 'sell': {
+            return {
+                ...state,
+                supplies: state.supplies - 20,
+                distance: state.distance,
+                days: state.days,
+                cash: state.cash + 5
+            };
+        }
+        case 'buy': {
+            return {
+                ...state,
+                supplies: state.supplies + 25,
+                distance: state.distance,
+                days: state.days,
+                cash: state.cash - 15
+            };
+        }
+        case 'theft': {
+            return {
+                ...state,
+                supplies: state.supplies + 25,
+                distance: state.distance,
+                days: state.days,
+                cash: state.cash * .5
+            }
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+let wagon = roadReducer(undefined, {});
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'travel', payload: 1 });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'gather' });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'tippedWagon' });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'travel', payload: 2 });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'theft' });
+console.log(wagon);
